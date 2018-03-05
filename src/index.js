a3449192267e2c3fce194dd4f3b959f323ba26e8
Ihar Papou <igarok-91@mail.ru>
2018-03-05 18:25:11 +0300

    task7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var i = 1;
+  var j = 0;
+
+  while (Math.pow(5, i) < number) {
+    j = j + Math.floor(number / Math.pow(5, i));
+    i++;
+  }
+  return j;
 }
